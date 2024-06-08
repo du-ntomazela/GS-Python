b6127ab98a82b816d96b84a6ce8d1147485b872d
du-ntomazela <161898042+du-ntomazela@users.noreply.github.com>
2024-06-07 23:23:50 -0300

    Otimização do código

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Função para verificar se o usuário digitou um número
 def func_numerico(msg):
     num = input(msg)
     while not num.isnumeric():
@@ -6,49 +7,64 @@ def func_numerico(msg):
     num = int(num)
     return num
 
+#Função para compara o input do usuário com as opções disponiveis
+def compare_list(l1, msg):
+    x = input(msg)
+    while x not in l1:
+        x = input(msg)
+    return x
 
-def func_continuar(msg):
-    continuar = input(msg)
-    while continuar != 's' and continuar != 'n':
-        print("Apenas s/n!")
-        continuar = input(msg)
-    return continuar
 
 
+# Lista dos paises presentes no projeto:
 paises = ['China', 'Indonésia', 'Estados Unidos']
 
-nome = input("Digite seu nome: ")
-senha = func_numerico("Digite sua senha: ")
-senhaCorreta = func_numerico("Repita sua senha: ")
+# Login do usuário:
+nome = input("Digite seu usuário: ")
+senha = func_numerico("Digite sua senha(apenas números): ")
+senhaCorreta = func_numerico("Repita sua senha(apenas números): ")
 while senha != senhaCorreta:
-    senha = func_numerico("Digite sua senha: ")
-    senhaCorreta = func_numerico("Repita sua senha: ")
-print(f"\nOlá {nome} seja bem vindo !\nSomos da BlueBacter !!")
-continuar = func_continuar("\nDeseja continuar ?")
-if continuar == 'n':
-    print("Obrigado pela visita!")
-else:
-    while True:
-        print("\nEstes são os países que mais poluem os oceanos do mundo:")
-        for i in range(len(paises)):
-            print(f"{paises[i]}")
-        info = input("\nDeseja obter informação de qual país ?")
-        while info not in paises:
-            print("Apenas os países da lista!")
-            info = input("Deseja obter informação de qual país ?")
-        if info == 'China':
-            print("A China é frequentemente apontada como um dos maiores contribuintes para a poluição marinha devido à"
-                  "sua grande população, atividade industrial e descargas de esgoto.")
-        elif info == 'Indonésia':
-            print("Com mais de 17.000 ilhas e uma população significativa, a Indonésia enfrenta desafios significativos"
-                  "de gestão de resíduos. O escoamento de resíduos plásticos dos rios\npara o oceano é um problema "
-                  "particularmente grave.")
-        else:
-            print("Com uma grande população costeira e uma economia industrial desenvolvida, os Estados Unidos também "
-                  "contribuem significativamente para a poluição dos oceanos,\nespecialmente devido ao escoamento de "
-                  "produtos químicos agrícolas, resíduos industriais e plásticos.")
-        continuar = func_continuar("\nDeseja continuar vendo as notícias ?")
-        if continuar == 'n':
-            print("\nCom base nessas informações, nossa empresa consegue atuar melhor com as bactérias que são "
-                  "capazes de decompor os plásticos nos oceanos. E fazendo está pesquisa, sabemos por onde começar !")
-            break
+    print("As senhas devem coincidir! Digite-as novamente.")
+    senha = func_numerico("Digite sua senha(apenas números): ")
+    senhaCorreta = func_numerico("Repita sua senha(apenas números): ")
+
+# Inicio do código com um "bem vindo"
+print(f"\nOlá {nome} seja bem vindo !\nSomos da BlueBacterium !!")
+continuar = "s"
+
+
+# Pergunta ao usuário sobre qual pais deseja ter as informações compartilhasdas.
+while continuar == "s":
+    print("\nEstes são os países que mais poluem os oceanos do mundo:")
+
+    #Mostrando os paises ao usuário:
+    for i in range(len(paises)):
+        print(f"{paises[i]}")
+    info = compare_list(paises, "\nDeseja obter informação de qual país ?")
+
+    # Informações sobre a China:
+    if info == 'China':
+        print("A China é frequentemente apontada como um dos maiores contribuintes para a poluição marinha devido à"
+                " sua grande população, atividade industrial e descargas de esgoto.")
+        print("")
+
+    #Informações sobre a Indonésia
+    elif info == 'Indonésia':
+        print("Com mais de 17.000 ilhas e uma população significativa, a Indonésia enfrenta desafios significativos"
+                "de gestão de resíduos. O escoamento de resíduos plásticos dos rios\npara o oceano é um problema "
+                "particularmente grave.")
+        print("")
+
+    #Informações sobre os Estados Unidos:
+    else:
+        print("Com uma grande população costeira e uma economia industrial desenvolvida, os Estados Unidos também "
+                "contribuem significativamente para a poluição dos oceanos,\nespecialmente devido ao escoamento de "
+                "produtos químicos agrícolas, resíduos industriais e plásticos.")
+        print("")
+    #Perguntando ao usuário se deseja continuar vendo as informações:
+    continuar = compare_list(["s", "n"], "Deseja continuar? \n(responda com s/n)\n ->")
+
+print("\nCom base nessas informações, nossa empresa consegue atuar melhor com as bactérias que são "
+      "capazes de decompor os plásticos nos oceanos. E fazendo esta pesquisa, sabemos por onde começar !")
+print("Obrigado pela visita!")
+
